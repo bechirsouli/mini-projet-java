aea59dba13fe91fdddb87bb00c9ae052c2a0c1c9
Bechir Souli <souli.bechir@gmail.com>
2023-11-10 22:09:41 +0100

    initialisaion du projet

---
+++ src/main/java/com/fst/applicationsrt/ApplicationsrtApplication.java
@@ -0,0 +1,14 @@
+package com.fst.applicationsrt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationsrtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationsrtApplication.class, args);
+		System.out.println("Bechir SOULI - Étudiant en mastère professionnelle SRT");
+	}
+
+}
